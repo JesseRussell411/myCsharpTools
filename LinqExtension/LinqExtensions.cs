23d80dbfef0388eb89fe03210f87f8d09e57fdea
Jesse Russell <jesserussell411@gmail.com>
2021-07-15 01:29:32 -0600

    spelling fix

--- LinqExtension/LinqExtensions.cs
+++ LinqExtension/LinqExtensions.cs
@@ -18,7 +18,7 @@ namespace JesseRussell.LinqExtension
         // Prints the called-on object with Console.WriteLine by default.
         public static void PrintSelf(this object self, Action<object> printFunction = null) => (printFunction ?? Console.WriteLine)(self);
 
-        // Produces a dictionary form a stream of KeyValuePairs
+        // Produces a dictionary from a stream of KeyValuePairs
         public static Dictionary<K, V> ToDictionary<K, V>(this IEnumerable<KeyValuePair<K, V>> stream) => stream.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
     }
 }
