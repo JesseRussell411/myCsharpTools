63283c8f65533afb594e24ac9c18faf14234abb8
Jesse Russell <jesserussell411@gmail.com>
2021-07-14 23:37:29 -0600

    stuff!!!!!!

--- FancyTyping/misc/NullChecked.cs
+++ FancyTyping/misc/NullChecked.cs
@@ -10,31 +10,22 @@ namespace JesseRussell.FancyTyping
     /// at instantiation, but sometimes it will occur later.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public readonly struct UnNullable<T>
+    public readonly struct NullChecked<T>
     {
-        /// <summary>
-        /// Unfortunately, It is possible for The value to be null if the UnNullable has been created using it's default constructor or the default keyword.
-        /// In this case, a NullReferenceException will be thrown, not at instantiation, but when the value is accessed. If desired, the user can access this field instead to override the null check.
-        /// </summary>
         public readonly T NullableValue;
         public T Value => NullableValue ?? throw new NullReferenceException();
 
-        public UnNullable(T value)
+        public NullChecked(T value)
         {
             NullableValue = value ?? throw new NullReferenceException();
         }
 
-        public static explicit operator UnNullable<T>(T value) => new UnNullable<T>(value);
-        public static implicit operator T(UnNullable<T> value) => value.Value;
+        public static implicit operator NullChecked<T>(T value) => new NullChecked<T>(value);
+        public static implicit operator T(NullChecked<T> value) => value.Value;
 
         // obligatory object methods:
         public override string ToString() => Value.ToString();
         public override int GetHashCode() => Value.GetHashCode();
         public override bool Equals(object obj) => Value.Equals(obj);
-    }
-
-    public static class UnNullableExtensions
-    {
-        public static UnNullable<T> ToUnNullable<T>(this T self) => new UnNullable<T>(self);
     }
 }
--- Net5TryMe/Net5TryMe.cs
+++ Net5TryMe/Net5TryMe.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using JesseRussell.FancyTyping;
 
 namespace Net5TryMe
 {
+    class Foo
+    {
+    }
     class Net5TryMe
     {
+        static void somethingWithFoo(NullChecked<Foo> foo)
+        {
+            Console.WriteLine("Doing something with foo...");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Foo f = null;
+
+            somethingWithFoo(f);
         }
     }
 }
