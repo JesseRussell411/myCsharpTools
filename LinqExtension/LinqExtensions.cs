da5ee5b501477221c0c1e34b75cfdf72c7b35ec6
Jesse Russell <jesserussell411@gmail.com>
2021-07-15 01:25:01 -0600

    added total common sense ToDictionary extension

--- LinqExtension/LinqExtensions.cs
+++ LinqExtension/LinqExtensions.cs
@@ -17,5 +17,8 @@ namespace JesseRussell.LinqExtension
 
         // Prints the called-on object with Console.WriteLine by default.
         public static void PrintSelf(this object self, Action<object> printFunction = null) => (printFunction ?? Console.WriteLine)(self);
+
+        // Produces a dictionary form a stream of KeyValuePairs
+        public static Dictionary<K, V> ToDictionary<K, V>(this IEnumerable<KeyValuePair<K, V>> stream) => stream.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
     }
 }
